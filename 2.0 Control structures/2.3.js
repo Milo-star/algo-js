5282e6287b3b9dc64361734b87afb8b357b2311a
Milo Bonnet <milovtt@gmail.com>
2022-11-24 10:28:54 +0100

    Premier push

---
+++ 2.0 Control structures/2.3.js
@@ -0,0 +1,17 @@
+let paire = 1;
+while (paire <= 100){
+    let x = paire%2;
+
+    if ( x == 0 ){
+        console.log(paire);
+    }
+    paire++;
+}
+
+for (let i = 1 ; i <= 50; i++){
+    let x = i%2;
+
+    if ( x == 0 ){
+        console.log(i);
+    }
+}
