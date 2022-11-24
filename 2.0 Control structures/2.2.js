7ff6c961bc3bd83f617bd71132d822ee3f08b77f
Milo Bonnet <milovtt@gmail.com>
2022-11-24 12:25:07 +0100

    modif

--- 2.0 Control structures/2.2.js
+++ 2.0 Control structures/2.2.js
@@ -1,14 +1,12 @@
 let petit = prompt("Entrer un nombre minimum");
 let grand = prompt("Entrer un nombre maximum");
+let current = prompt("Entrer un nouveau nombre.");
 
 if (petit > grand) {
     
     console.log("Je pense que tu n'as pas compris, Ciao.");
 
 } else {
-
-    let current = prompt("Entrer un nouveau nombre.");
-    
     if ( petit <= current <= grand ) {
 
         console.log(current + " est entre " + petit + " et " + grand);
